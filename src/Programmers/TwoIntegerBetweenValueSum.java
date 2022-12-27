3cbddc143729cede483ac21856b9d3fcc8530f13
endlessmomo <soohyuk96@naver.com>
2022-12-27 19:13:26 +0900

    두 정수 사이의 합

---
+++ src/Programmers/TwoIntegerBetweenValueSum.java
@@ -0,0 +1,19 @@
+package Programmers;
+
+import java.util.stream.IntStream;
+
+public class TwoIntegerBetweenValueSum {
+    public static void main(String[] args) {
+        System.out.println(solution(3, 5));
+    }
+
+    public static int solution(int x, int y){
+        if(x > y){
+            int tmp = x;
+            x = y;
+            y = tmp;
+        }
+
+        return IntStream.rangeClosed(x,y).sum();
+    }
+}
