dddea5257d172952b6aab5627d6fed4e953d08b2
endlessmomo <soohyuk96@naver.com>
2022-12-28 16:05:11 +0900

    음양 더하기

---
+++ src/Programmers/addMinusPlus.java
@@ -0,0 +1,18 @@
+package Programmers;
+
+public class addMinusPlus {
+    public static void main(String[] args) {
+        int[] absolutes = {4,7,12};
+        boolean[] signs = {true,false,true};
+
+        System.out.println(solution(absolutes, signs));
+    }
+
+    public static int solution(int[] absolutes, boolean[] signs){
+        int sum = 0;
+        for(int i = 0; i < absolutes.length; i++){
+            sum += signs[i] ? absolutes[i] : -absolutes[i];
+        }
+        return sum;
+    }
+}
