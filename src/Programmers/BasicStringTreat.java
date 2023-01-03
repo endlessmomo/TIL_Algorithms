041a47a0ba872fdbb5db0a809f9fadd8f0eb637c
endlessmomo <soohyuk96@naver.com>
2023-01-03 22:09:57 +0900

    문자열 다루기 기본

--- src/Programmers/BasicStringTreat.java
+++ src/Programmers/BasicStringTreat.java
@@ -5,12 +5,15 @@ public class BasicStringTreat {
         System.out.println(solution("a1234"));
     }
 
-    public static boolean solution(String s){
-       for(char c : s.toCharArray()){
-           if(!Character.isDigit(c)){
-               return false;
-           }
-       }
-       return true;
+    public static boolean solution(String s) {
+        if (s.length() == 4 || s.length() == 6) {
+            for (char c : s.toCharArray()) {
+                if (!Character.isDigit(c)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
     }
 }
