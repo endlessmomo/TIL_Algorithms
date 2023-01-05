13f7cf43a2a10b9001ec58d376a51837a2a1483c
endlessmomo <soohyuk96@naver.com>
2023-01-05 23:44:34 +0900

    최소직사각형

---
+++ src/Programmers/Lv1/Square.java
@@ -0,0 +1,16 @@
+package Programmers.Lv1;
+
+import java.util.Arrays;
+
+public class Square {
+    public static void main(String[] args) {
+        int[][] size = {{60,50}, {30,70}, {60,30}, {80,40}};
+        System.out.println(solution(size));
+    }
+
+    public static int solution(int[][] size){
+        return Arrays.stream(size).reduce((a,b) -> new int[]{
+                Math.max(Math.max(a[0], a[1]), Math.max(b[0], b[1])), Math.max(Math.min(a[0],a[1]), Math.min(b[0],b[1]))
+        }).map(it -> it[0] * it[1]).get();
+    }
+}
