cc72edc389cce2b073abc136845bc5a6cf2e919a
endlessmomo <soohyuk96@naver.com>
2023-01-15 23:40:10 +0900

    [1차] 비밀지도

--- src/Programmers/Lv1/SecretMap.java
+++ src/Programmers/Lv1/SecretMap.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 public class SecretMap {
     public static void main(String[] args) {
         System.out.println(Arrays.toString(solution(5, new int[]{9, 20, 28, 18, 11}, new int[]{30, 1, 21, 17, 28})));
+        System.out.println(Arrays.toString(solution(6, new int[]{46, 33, 33, 22, 31, 50}, new int[]{27, 56, 19, 14, 14, 10})));
+        System.out.println(Integer.toBinaryString(22 | 14));
     }
 
     private static String[] solution(int n, int[] arr1, int[] arr2) {
@@ -12,8 +14,11 @@ public class SecretMap {
 
         for (int i = 0; i < n; i++) {
              map[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
-             map[i].replaceAll("1", "#");
-             map[i].replaceAll("0", " ");
+             if(map[i].length() != n){
+                 map[i] = "0".repeat(n - map[i].length()) + map[i];
+             }
+             map[i] = map[i].replaceAll("1", "#");
+             map[i] = map[i].replaceAll("0", " ");
         }
 
         return map;
