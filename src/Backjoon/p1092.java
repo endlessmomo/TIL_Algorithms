0cbb5b290404bf6568a9347d8caa43969215db40
endlessmomo <soohyuk96@naver.com>
2023-02-18 23:00:30 +0900

    백준 항구 1092번 문제

--- src/Backjoon/p1092.java
+++ src/Backjoon/p1092.java
@@ -9,20 +9,25 @@ public class p1092 {
         Scanner sc = new Scanner(System.in);
 
         int crane_c = sc.nextInt();
-        String crane_s = sc.next();
+        sc.nextLine();
+        String crane_s = sc.nextLine();
 
         int box_c = sc.nextInt();
-        String box_s = sc.next();
+        sc.nextLine();
+        String box_s = sc.nextLine();
         System.out.println(solution(crane_c, crane_s, box_c, box_s));
     }
 
     public static int solution(int crane_c, String crane_s, int box_c, String box_s) {
-        List <Integer> crane_L = Arrays.stream(crane_s.split("")).map(Integer::valueOf).collect(Collectors.toList());
-        List <Integer> box_L = Arrays.stream(box_s.split("")).map(Integer::valueOf).collect(Collectors.toList());
+        List <Integer> crane_L = Arrays.stream(crane_s.split(" ")).map(Integer::valueOf).collect(Collectors.toList());
+        List <Integer> box_L = Arrays.stream(box_s.split(" ")).map(Integer::valueOf).collect(Collectors.toList());
 
         crane_L.sort(Comparator.reverseOrder());
         box_L.sort(Comparator.reverseOrder());
 
+        if(crane_L.get(0) < box_L.get(0)){
+            return -1;
+        }
         int res = 0;
 
         while (!box_L.isEmpty()) {
