8cecf3948ecb76c5b30f5e262883dcf877fe164c
endlessmomo <soohyuk96@naver.com>
2023-02-22 16:24:10 +0900

    프로그래머스 완주하지 못한 선수

---
+++ src/Programmers/Lv1/Marathon.java
@@ -0,0 +1,38 @@
+package Programmers.Lv1;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class Marathon {
+    public static void main(String[] args) {
+        String[] participant = new String[]{"leo", "kiki", "eden"};
+        String[] completion = new String[]{"eden", "kiki"};
+
+        System.out.println(solution(participant, completion));
+    }
+
+    public static String solution(String[] participant, String[] completion) {
+        Map <String,Integer> map = new HashMap <>();
+        String res = "";
+
+        for (int i = 0; i < participant.length; i++) {
+            map.put(participant[i], map.getOrDefault(participant[i], 0) + 1);
+        }
+
+        for (String key : completion) {
+            map.put(key, map.get(key) - 1);
+        }
+
+        Iterator <Map.Entry <String,Integer>> iter = map.entrySet().iterator();
+
+        while (iter.hasNext()) {
+            Map.Entry <String,Integer> entry = iter.next();
+            if (entry.getValue() != 0) {
+                res = entry.getKey();
+                return res;
+            }
+        }
+        return null;
+    }
+}
