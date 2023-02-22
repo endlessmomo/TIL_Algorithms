2a989d8106acbe0bb4ccbbaa5c2f89ab7ce06ebf
endlessmomo <soohyuk96@naver.com>
2023-02-22 16:54:33 +0900

    프로그래머스 부분 문자열 크기 비교

---
+++ src/Programmers/Lv1/LittleSIzeOfString.java
@@ -0,0 +1,19 @@
+package Programmers.Lv1;
+
+public class LittleSIzeOfString {
+    public static void main(String[] args) {
+        System.out.println(solution("3141592", "271"));
+    }
+
+    public static int solution(String s, String p){
+        int cnt = 0;
+        int len = p.length();
+        Long value = Long.parseLong(p);
+        for(int i = 0; i < s.length() - len + 1; i++){
+            if(Long.parseLong(s.substring(i, i+len)) <= value){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
