5a315e27b2d7de582cddc34a34e43ccb37cdba3b
endlessmomo <soohyuk96@naver.com>
2023-02-23 19:57:21 +0900

    프로그래머스 cache

--- src/Programmers/Lv2/cache.java
+++ src/Programmers/Lv2/cache.java
@@ -5,9 +5,10 @@ import java.util.Queue;
 
 public class cache {
     public static void main(String[] args) {
-        String[] cities = new String[]{"a", "b", "c", "a", "b"};
+       String[] cities = new String[]{"Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"};
+//        String[] cities = new String[]{"Seoul","Seoul","Seoul"};
 
-        System.out.println(solution(2, cities));
+        System.out.println(solution2(3, cities));
     }
 
     public static int solution(int cacheSize, String[] cities) {
@@ -25,11 +26,36 @@ public class cache {
                 res += 1;
                 cache.add(city);
             } else {
-                res+=5;
+                res += 5;
 
+                if (cache.size() >= cacheSize) {
+                    cache.remove();
+                }
+                cache.add(city);
+            }
+        }
+        return res;
+    }
+
+    public static int solution2(int cacheSize, String[] cities) {
+        Queue <String> cache = new LinkedList <>();
+        int res = 0;
+
+        if (cacheSize == 0) {
+            return cities.length * 5;
+        }
+
+        for (int i = 0; i < cities.length; i++) {
+            String city = cities[i].toLowerCase();
+            if(cache.contains(city)){
+                cache.remove(city);
+                cache.add(city);
+                res += 1;
+            } else {
                 if(cache.size() >= cacheSize){
                     cache.remove();
                 }
+                res += 5;
                 cache.add(city);
             }
         }
