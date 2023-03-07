50eb45e4e4e0c354c9b7480a4edf9ee8cee21881
endlessmomo <soohyuk96@naver.com>
2023-03-07 23:28:12 +0900

    프로그머스 명예의 전당

---
+++ src/Programmers/Lv1/HallOfFame.java
@@ -0,0 +1,29 @@
+package Programmers.Lv1;
+
+import java.util.*;
+
+public class HallOfFame {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(3, new int[] {10, 100, 20, 150, 1, 100, 200})));
+        System.out.println(Arrays.toString(solution(4, new int[] {0, 300, 40, 300, 20, 70, 150, 50, 500, 1000})));
+    }
+
+    public static int[] solution(int k, int[] score){
+        PriorityQueue<Integer> heap = new PriorityQueue();
+        List <Integer> res = new ArrayList <>();
+
+        for(int value : score) {
+            if(heap.size() == k){
+                heap.add(value);
+                heap.remove();
+                res.add(heap.peek());
+                continue;
+            }
+
+            heap.add(value);
+            res.add(heap.peek());
+        }
+
+        return res.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
