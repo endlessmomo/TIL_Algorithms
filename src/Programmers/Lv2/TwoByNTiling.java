d7aa3899880813595195fea12f9852c2610bdf2e
endlessmomo <soohyuk96@naver.com>
2023-03-13 17:40:23 +0900

    프로그머스 2 x n 타일링

---
+++ src/Programmers/Lv2/TwoByNTiling.java
@@ -0,0 +1,23 @@
+package Programmers.Lv2;
+
+public class TwoByNTiling {
+    public static void main(String[] args) {
+        System.out.println(solution(5));
+    }
+
+    static int[] dp;
+
+    public static int solution(int n) {
+        dp = new int[n + 1];
+
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
+        }
+
+        return dp[n];
+    }
+}
