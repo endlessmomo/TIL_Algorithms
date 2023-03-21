eb3871c9cc27ae01f30062f6517ef87295426138
endlessmomo <soohyuk96@naver.com>
2023-03-21 22:06:07 +0900

    프로그머스  이중우선순위큐

---
+++ src/Programmers/lv3/GoToSchool.java
@@ -0,0 +1,16 @@
+//package Programmers.lv3;
+//
+//public class GoToSchool {
+//    public static void main(String[] args) {
+//        int m = 4;
+//        int n = 3;
+//        int[][] puddles = {{2, 2}};
+//
+//        System.out.println(solution(m, n, puddles));
+//
+//    }
+//
+//    public static int solution(int m, int n, int[][] puddles) {
+//
+//    }
+//}
