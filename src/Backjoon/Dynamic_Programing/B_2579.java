5f33369371811a1c5893a53c6df48155953fe7ec
endlessmomo <soohyuk96@naver.com>
2023-05-30 19:59:36 +0900

    ADD : [DP] 백준 2579번 풀이

--- src/Backjoon/Dynamic_Programing/B_2579.java
+++ src/Backjoon/Dynamic_Programing/B_2579.java
@@ -10,18 +10,21 @@ public class B_2579 {
         Integer floorCnt = Integer.parseInt(in.readLine());
 
         int[] dp = new int[floorCnt + 1];
-        int[] values = new int[floorCnt];
+        int[] values = new int[floorCnt + 1];
 
-        for (int i = 0; i < floorCnt; i++) {
+        for (int i = 1; i <= floorCnt; i++) {
             values[i] = Integer.parseInt(in.readLine());
         }
 
         dp[0] = 0;
         dp[1] = values[1];
-        dp[2] = values[2];
 
-        for(int i = 3; i < dp.length; i++){
-            dp[i] = Math.max(dp[i-3] + dp[i-1] + dp[i], dp[i-2] + dp[i]);
+        if(floorCnt >= 2){
+            dp[1] = values[1] + values[2];
+        }
+
+        for (int i = 3; i < dp.length; i++) {
+            dp[i] = Math.max(dp[i - 3] + values[i - 1] + values[i], dp[i - 2] + values[i]);
         }
         System.out.println(dp[floorCnt]);
     }
