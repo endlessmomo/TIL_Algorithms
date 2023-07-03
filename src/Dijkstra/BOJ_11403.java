762858e02de8fa19a330c9d0049e9a1a7265583a
endlessmomo <soohyuk96@naver.com>
2023-07-03 09:16:17 +0900

    UPDATE : [Dijkstra] 백준 11403번 풀이

--- src/Dijkstra/BOJ_11403.java
+++ src/Dijkstra/BOJ_11403.java
@@ -5,6 +5,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/*
+* 1. 문제 이해
+* i에서 j로 가는 길이 존재하면 1, 존재하지 않으면 0k
+*
+* 2. 아이디어
+* 단일 방향으로 흐르기 때문에, 반복문을 통한 route가 있는지 여부를 파악한다.
+*
+* 3. 어려움 및 해결 방식
+* 없음
+* */
 public class BOJ_11403 {
     static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
